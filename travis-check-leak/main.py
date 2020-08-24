aa5ca96c3fd3fcbb169405aefaad23005f93ceff
louiscklaw <louiscklaw@gmail.com>
2020-08-24 22:30:59 +0800

    update and test,

--- travis-check-leak/main.py
+++ travis-check-leak/main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# coding:utf-8
 
 import os
 import sys
@@ -9,6 +8,7 @@ from pprint import pprint
 import shlex
 
 from subprocess import run, PIPE, Popen
+from parseCredentialFile import *
 
 def checkPythonVersion():
   import platform
@@ -46,32 +46,6 @@ def checkLeak(should_not_appear, filepath_to_check):
     print(result.stdout)
     # print('{}:{}, {}, {}'.format(word, result.returncode, result.stdout, result.args))
     raise 'leakage found'
-
-def readCredentialFile(filepath):
-  file_content = open(filepath,'r').readlines()
-  file_content = filter(lambda x: x.strip(), file_content)
-  file_content = filter(lambda x: x[0] != '#', file_content)
-
-  return set(file_content)
-
-def parseCredentialFile():
-  try:
-    filepath='/home/logic/.credentials.rc'
-    if os.getenv('CI',False):
-      # for checking happening on travis
-      # TODO: better handling on this
-      env_names = ['DOCKERHUB_PASSWORD', 'DOCKERHUB_USER', 'GITHUB_TOKEN', 'SLACK_TOKEN', 'TRAVIS_AUTH_TOKEN', 'TRAVIS_TOKEN']
-      return ['{}={}'.format(env_name, os.environ[env_name]) for env_name in env_names]
-    else:
-      if os.path.exists(filepath):
-
-        content = readCredentialFile(filepath)
-        content = map(lambda x: x.strip(), content)
-        return map(lambda x: x.replace('export ',''), content)
-      else:
-        raise 'credentials filepath not found'
-  except Exception as e:
-    raise e
 
 def clearBashValue(txt_value_in):
   output = txt_value_in
