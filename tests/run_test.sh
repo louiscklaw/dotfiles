f62f7aa255111b6fff03ebd9d330772249b90433
louiscklaw <louiscklaw@gmail.com>
2020-08-29 19:31:22 +0800

    update and test,

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -5,6 +5,18 @@
 
 set -ex
 
+# docker kill $(docker ps -qa)
+
+docker container prune -f
+
 docker build -t logickee/ubuntu-1804-zsh .
 
+rm -rf /home/logic/_workspace/dotfiles/logs/*.log
+
 cat tests/full_docker_commands.sh | while read i; do printf "%q\n" "$i"; done | xargs --max-procs=99 -I CMD bash -c CMD
+
+echo "---------------------------------"
+echo " test done and  prune containers "
+echo "---------------------------------"
+
+docker container prune -f
