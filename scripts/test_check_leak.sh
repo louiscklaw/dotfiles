3b5b55e2cd591e120031c901cc4fe35bd472b5ae
louiscklaw <louiscklaw@gmail.com>
2020-09-04 19:42:22 +0800

    ci: update,

--- scripts/test_check_leak.sh
+++ scripts/test_check_leak.sh
@@ -2,40 +2,14 @@
 
 set -ex
 
-# apt update
-# apt install -y python3 python3-pip python3-dev python3-wheel python3-setuptools
-# python3 -m pip install pipenv
+TMP_DIR=$(mktemp -d)
 
-# export PATH=$PATH:/root/.local/bin
-# export LC_ALL=C.UTF-8
-# export LANG=C.UTF-8
+rsync -avzh travis-check-leak/ $TMP_DIR
 
-mkdir -p tmp
+cd $TMP_DIR
 
-cd tmp
-  # wget pipfile
-  # wget check-leak.py
-
-  # old check leak
-  # cp ../travis-check-leak/Pipfile .
-  # cp ../travis-check-leak/Pipfile.lock .
-  # cp ../travis-check-leak/check-leak.py .
-  # cp ../travis-check-leak/test_pass.py .
-
-  # cp ../travis-check-leak/leak.js .
-
-  # pipenv --rm || true
-  # pipenv sync
-  # pipenv run python3 test_pass.py
-
-
-  rsync -avzh ../travis-check-leak/ .
-  # pipenv --rm || true
-  # pipenv sync
-
-  # usecase
   pipenv sync
-  # pipenv run python3 main.py
-  pipenv run python3 main.py $@
+
+  pipenv run python3 main.py
 
 cd ..
