77d1700ebb3489cf54350cc66fb035c0cd93f4d4
louiscklaw <logickee@gmail.com>
2022-01-25 11:04:52 +0800

    update,

---
+++ git/gitPushNewBranch.cmd
@@ -0,0 +1,11 @@
+
+SET _c=
+FOR /F "tokens=4 delims= " %%G IN ('git branch') DO (
+    IF DEFINED _c <nul set /p z=", "
+    <nul set /p z=%%G
+    SET _c=1
+)
+
+
+git branch | findstr *
+@REM git push --set-upstream origin %*
