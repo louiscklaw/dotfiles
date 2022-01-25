e5243e434116d1a1abc3a77b2168632f84a4674f
louiscklaw <logickee@gmail.com>
2022-01-25 22:56:18 +0800

    update,

---
+++ initBootstrap.bat
@@ -0,0 +1,33 @@
+@REM Fix for PowerShell Script Not Digitally Signed
+@REM Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+@REM Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco install git -v -y
+choco install vscode -v -y
+choco install whatsapp -v -y
+choco install visualstudio2022community -v -y
+choco install virtualbox -v -y
+choco install telegram -v -y
+choco install slack -v -y
+choco install signal -v -y
+choco install rpi-imager -v -y
+choco install python3 -v -y
+choco install python3 -v -y
+choco install prusaslicer -v -y
+choco install powershell.portable -v -y
+choco install picpick.portable -v -y
+choco install oh-my-posh -v -y
+choco install nodejs --version=14.18.3 -v -y
+choco install git -v -y
+choco install firacode -v -y
+choco install filezilla -v -y
+choco install figma -v -y
+choco install figma -v -y
+choco install dellcommandupdate -v -y
+choco install curl -v -y
+choco install cmder -v -y
+choco install autohotkey.portable -v -y
+choco install aria2 -v -y
+choco install 7zip.install -v -y
+choco install 7zip -v -y
+
---
+++ initWorkSpace.cmd
@@ -0,0 +1,8 @@
+
+pushd c:\Users\logic\_workspace
+  git clone git@github.com:louiscklaw/kicad-playlist.git
+  git clone git@github.com:louiscklaw/lynked-demo-tryout.git
+  git clone git@github.com:louiscklaw/react-playlist.git
+  git clone git@github.com:louiscklaw/hugo-playlist.git
+  
+popd
