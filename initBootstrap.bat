7025cb58e906bb7ba8e3a9ffb926843f846ab451
louiscklaw <logickee@gmail.com>
2022-01-28 14:06:28 +0800

    update windows dotfiles,

--- initBootstrap.bat
+++ initBootstrap.bat
@@ -36,3 +36,4 @@ choco install 7zip -v -y
 choco install streamlabs-obs -v -y
 choco install discord -v -y
 choco install hwmonitor -v -y
+choco install jetbrainsmono -v -y
--- syncDotFiles.cmd
+++ syncDotFiles.cmd
@@ -1,6 +1,10 @@
 
 pushd c:\Users\logic\dotfiles
+  git add . 
+  git commit -m"update windows dotfiles,"
+
   git fetch --all
   git pull
+
   git push
 popd
