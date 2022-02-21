7f8606f8be79017fc7452d58dd8d626e872c49d3
Louis Law <louiscklaw@users.noreply.github.com>
2022-02-20 18:07:55 -0800

    update windows dotfiles,

--- initBootstrap.bat
+++ initBootstrap.bat
@@ -3,11 +3,8 @@
 : Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 Set-ExecutionPolicy Bypass
-Get-AppPackage *SkypeApp* | Remove-AppPackage
-Get-AppPackage *Wallet* | Remove-AppPackage
 
 choco feature enable -n allowGlobalConfirmation
-
 
 choco install git -v -y
 choco install vscode -v -y
@@ -20,17 +17,12 @@ choco install telegram -v -y
 choco install slack -v -y
 choco install signal -v -y
 choco install rpi-imager -v -y
-choco install python3 -v -y
-choco install python3 -v -y
+@REM choco install python3 -v -y
 choco install prusaslicer -v -y
-choco install powershell.portable -v -y
 choco install picpick.portable -v -y
-choco install oh-my-posh -v -y
-choco install nodejs --version=14.18.3 -v -y
-choco install git -v -y
+@REM choco install nodejs --version=14.18.3 -v -y
 choco install firacode -v -y
 choco install filezilla -v -y
-choco install figma -v -y
 choco install figma -v -y
 choco install dellcommandupdate -v -y
 choco install curl -v -y
@@ -38,17 +30,13 @@ choco install cmder -v -y
 choco install autohotkey.portable -v -y
 choco install aria2 -v -y
 choco install 7zip.install -v -y
-choco install 7zip -v -y
-
 choco install streamlabs-obs -v -y
 choco install discord -v -y
 : clist -l | findstr discord
-
 choco install hwmonitor -v -y
 choco install jetbrainsmono -v -y
 choco install obs-studio -v -y
 choco install androidstudio -v -y
-
 choco install winsshterm -v -y
 choco install putty -v -y
 choco install winscp -v -y
@@ -58,13 +46,13 @@ choco install wechat -v -y
 choco install deskpins -v -y
 choco install vlc -v -y
 choco install gnuwin32-coreutils.portable -v -y
-choco install aria2 -v -y
 choco install xnviewmp.install -v -y
 choco install httpie -v -y
-
 @REM C:\tools\msys64\usr\bin
 @REM C:\tools\msys64\mingw64\bin
 choco install msys2 -v -y
+
+choco install jdk8 -v -y
 
 
 powercfg.exe /hibernate off
