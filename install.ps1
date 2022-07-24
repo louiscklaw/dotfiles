057e6a119cf4c3358430dd3269995dafedea5362
Louis Law <louiscklaw@users.noreply.github.com>
2022-07-24 21:08:57 +0800

    update windows dotfiles,

--- install.ps1
+++ install.ps1
@@ -1,3 +1,5 @@
-ln -s %UserProfile%\Documents\Windows­PowerShell\profile.ps1
+# ln -s %UserProfile%\Documents\Windows­PowerShell\profile.ps1
 
-mklink /d  .\profile.ps1 .\Documents\Windows­PowerShell\profile.ps1
+# mklink /d  .\profile.ps1 .\Documents\Windows­PowerShell\profile.ps1
+
+copy _install_profile.ps1 "C:\Users\logic\Documents\WindowsPowerShell\profile.ps1"
