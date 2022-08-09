e1deac81226a4ea69f35b67bc00d14b573999348
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-09 16:44:40 +0800

    update HOST_ADDRESS,

--- docker.ps1
+++ docker.ps1
@@ -6,9 +6,15 @@ function dockerRun(){
   $curDir_temp = Get-Location
   echo $curDir_temp
 
+  $var1 = ipconfig |findstr IPv4 |findstr 192
+  $HOST_ADDRESS=$var1.Split(" ")[16]
+
+  echo $HOST_ADDRESS
+
   docker run -it `
     -v ${curDir_temp}:/app `
     -w /app `
+    --env HOST_ADDRESS=$HOST_ADDRESS `
     -v /var/run/docker.sock:/var/run/docker.sock `
     -p 3000-3099:3000 `
     -p 5555-5575:5555 `
@@ -24,7 +30,11 @@ function dockerRunPython {dockerRun python:latest bash}
 
 function dockerRunNode {dockerRun node:latest bash}
 function dockerRunNode18 {dockerRun node:18-buster bash}
-function dockerRunNode16 {dockerRun node:16-buster bash}
+function dockerRunNode16 {
+  
+  echo $var1
+  dockerRun node:16-buster bash
+}
 function dockerRunNode14 {dockerRun node:14-buster bash}
 function dockerRunNode14 {dockerRun node:14-buster bash}
 
