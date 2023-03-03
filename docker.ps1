1a910e61903fbd36caf63ace1d59bb3e87701d89
louiscklaw <logickee@gmail.com>
2023-03-03 23:36:39 +0800

    update format,

--- docker.ps1
+++ docker.ps1
@@ -1,13 +1,13 @@
-function echoPwd(){
+function echoPwd() {
   echo (Get-Location)
 }
 
-function dockerRun(){
+function dockerRun() {
   $curDir_temp = Get-Location
   echo $curDir_temp
 
-  $var1 = ipconfig |findstr IPv4 |findstr 192
-  $HOST_ADDRESS=$var1.Split(" ")[16]
+  $var1 = ipconfig | findstr IPv4 | findstr 192
+  $HOST_ADDRESS = $var1.Split(" ")[16]
 
   echo $HOST_ADDRESS
 
@@ -23,19 +23,19 @@ function dockerRun(){
     $args
 }
 
-function dockerRunUbuntu1804 {dockerRun ubuntu:18.04 bash}
-function dockerRunUbuntu {dockerRun ubuntu:latest bash}
-function dockerRunFedora {dockerRun fedora:latest bash}
-function dockerRunPython {dockerRun python:latest bash}
+function dockerRunUbuntu1804 { dockerRun ubuntu:18.04 bash }
+function dockerRunUbuntu { dockerRun ubuntu:latest bash }
+function dockerRunFedora { dockerRun fedora:latest bash }
+function dockerRunPython { dockerRun python:latest bash }
 
-function dockerRunNode {dockerRun node:latest bash}
-function dockerRunNode18 {dockerRun node:18-buster bash}
+function dockerRunNode { dockerRun node:latest bash }
+function dockerRunNode18 { dockerRun node:18-buster bash }
 function dockerRunNode16 {
   
   echo $var1
   dockerRun node:16-buster bash
 }
-function dockerRunNode14 {dockerRun node:14-buster bash}
-function dockerRunNode14 {dockerRun node:14-buster bash}
+function dockerRunNode14 { dockerRun node:14-buster bash }
+function dockerRunNode14 { dockerRun node:14-buster bash }
 
-function dockerRunNest16 {dockerRun logickee/docker_nest16_tryout bash}
+function dockerRunNest16 { dockerRun logickee/docker_nest16_tryout bash }
