ae8056bfee465bd7c5100b851ff3c4c2954844b6
louiscklaw <louiscklaw@users.noreply.github.com>
2023-05-11 17:27:58 +0800

    update fedora,

--- tests/installDocker.sh
+++ tests/installDocker.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
-
+#
 set -ex
 
-cd /root
-  source .louis_alias
+ sudo dnf remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine
 
-  echo 'testing install "installDocker"'
-  installDocker
-  installHelloworld
+sudo dnf -y install dnf-plugins-core
 
-  echo 'testing install done'
+sudo dnf config-manager \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
+
+sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo systemctl start docker
+
+sudo docker run hello-world
+
