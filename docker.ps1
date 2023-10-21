d132cd6db17b4e5fbd7408f10521e233349d816e
louiscklaw <louiscklaw@users.noreply.github.com>
2023-10-21 16:31:57 +0800

    update,

--- docker.ps1
+++ docker.ps1
@@ -27,13 +27,9 @@ function dockerRunFedora { dockerRun fedora:latest bash }
 function dockerRunPython { dockerRun python:latest bash }
 
 function dockerRunNode { dockerRun node:latest bash }
-function dockerRunNode18 { dockerRun node:18-buster bash }
-function dockerRunNode16 {
-  
-  echo $var1
-  dockerRun node:16-buster bash
-}
-function dockerRunNode14 { dockerRun node:14-buster bash }
-function dockerRunNode14 { dockerRun node:14-buster bash }
+function dockerRunNode20 { dockerRun node:20-bullseye bash }
+function dockerRunNode18 { dockerRun node:18-bullseye bash }
+function dockerRunNode16 { dockerRun node:16-bullseye bash }
+function dockerRunNode14 { dockerRun node:14-bullseye bash }
 
 function dockerRunNest16 { dockerRun logickee/docker_nest16_tryout bash }
